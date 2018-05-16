4cb208d2d10cd08cbe426a3a55a168782ca173af
Martin Dias <tinchodias@gmail.com>
2018-05-16 20:48:28 -0300

    Some enhancements
    * Remove "ver"... makes no sense
    * Unify some vocabulary in controls
    * Unify some variable names
    * Personalize bootbox buttons

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,7 +2,8 @@ class LibrosController {
     constructor(librosService, growl) {
         this.libros = []
         this.librosService = librosService
-        this.libroSeleccionado = null
+        this.libroParaModificar = null
+        this.libroParaAgregar = null
         this.growl = growl
         this.errorHandler = (response) => {
             this.notificarError(response.data.error)
@@ -28,46 +29,53 @@ class LibrosController {
 
     // AGREGAR
     agregarLibro() {
-        this.librosService.agregar(this.nuevoLibro).then((response) => {
-            this.notificarMensaje('Libro agregado con id:' + response.data.id)
+        this.librosService.agregar(this.libroParaAgregar).then((response) => {
+            this.notificarMensaje("¡Libro agregado con # " + response.data.id + "!")
             this.actualizarLista()
-            this.nuevoLibro = null
+            this.libroParaAgregar = null
         }, this.errorHandler)
     }
 
     // ELIMINAR
     eliminar(libro) {
-        const mensaje = "¿Está seguro de eliminar: '" + libro.titulo + "'?"
-        bootbox.confirm(mensaje, (confirma) => {
-            if (confirma) {
-                this.librosService.eliminar(libro).then(() => {
-                    this.notificarMensaje('Libro eliminado!')
-                    this.actualizarLista()
-                }, this.errorHandler)
-            }
-        })
+        const mensaje = "¿Está seguro de eliminar el libro titulado '" + libro.titulo + "' de '" + libro.autor + "'?"
+        bootbox.confirm({
+            message: mensaje, 
+            buttons: {
+                confirm: {
+                    label: 'Sí, eliminar',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'Cancelar',
+                    className: 'btn-danger'
+                }
+            },
+            callback: (confirma) => {
+                if (confirma) {
+                    this.librosService.eliminar(libro).then(() => {
+                        this.notificarMensaje('¡Libro eliminado!')
+                        this.actualizarLista()
+                    }, this.errorHandler)
+                }
+            }})
     }
 
-    verDetalle(libro) {
-        this.libroSeleccionado = libro
-        $("#verLibroModal").modal({})
-    }
-
-    // EDITAR
-    editarLibro(libro) {
+    // MODIFICAR
+    modificarLibro(libro) {
         // Copiamos al libro porque sino al cerrar el diálogo queda modificado en la lista
-    	this.libroSeleccionado = {...libro}
-        $("#editarLibroModal").modal({})
+    	this.libroParaModificar = {...libro}
+        $("#modificarLibroModal").modal({})
     }
 
-    guardarLibro() {
-        this.librosService.modificar(this.libroSeleccionado).then(() => {
-            this.notificarMensaje('Libro modificado!')
+    aplicarModificacion() {
+        this.librosService.modificar(this.libroParaModificar).then(() => {
+            this.notificarMensaje('¡Libro modificado!')
             this.actualizarLista()
         }, this.errorHandler)
 
-        this.libroSeleccionado = null
-        $("#editarLibroModal").modal('toggle')
+        this.libroParaModificar = null
+        $("#modificarLibroModal").modal('toggle')
     }
 
 }
